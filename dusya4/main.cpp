4623ede770b0ec74c916b586cdb32860889a3d25
Eudokia Butuzova <eudokia@bk.ru>
2016-10-25 23:32:34 +0300

    New Programm_4

---
+++ dusya4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {int c=1,n,k=0,i,j;
+cin>>n;
+for(i=0;i<=n;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        if(i%j==0) {k++;}
+    }
+    if(k==2&&n%i!=0) {c++;}
+    k=0;
+}
+cout<<c<<endl;
+    return 0;
+}
+
