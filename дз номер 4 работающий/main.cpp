f53a4a30b78e856ece727836687c84073a21d6e3
Eudokia Butuzova <eudokia@bk.ru>
2016-12-11 00:01:43 +0300

    дз номер 4 нормальный вариант

---
+++ дз номер 4 работающий/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+int n,k;
+cin>>n;
+cin>>k;
+int mass[n];
+for(int i=0;i<n;i++)
+    cin>>mass[i];
+    sort(mass,mass+n);
+    cout<<mass[k-1];
+}
