a9d01e4c0b52bc9ec37c6383e32987e7c6ab2a90
Eudokia Butuzova <eudokia@bk.ru>
2016-12-11 00:31:07 +0300

    дз номер 2

--- дз номер 4 работающий/main.cpp
+++ дз номер 4 работающий/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 using namespace std;
-int main() {
-int n,k;
-cin>>n;
-cin>>k;
-int mass[n];
-for(int i=0;i<n;i++)
-    cin>>mass[i];
-    sort(mass,mass+n);
-    cout<<mass[k-1];
+int main(){
+    int i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12;
+    for(i1=0;i1<10;i1++){
+    for(i2=0;i2<10;i2++){
+    for(i3=0;i3<10;i3++){
+    for(i4=0;i4<10;i4++){
+    for(i5=0;i5<10;i5++){
+    for(i6=0;i6<10;i6++){
+    for(i7=0;i7<10;i7++){
+    for(i8=0;i8<10;i8++){
+    for(i9=0;i9<10;i9++){
+    for(i10=0;i10<10;i10++){
+    for(i11=0;i11<10;i11++){
+    for(i12=0;i12<10;i12++){
+            if (((i1+i2+i3)%2==0)&&((i10+i11+i12)%3==0)&&(9i3+i6+i9+i12)%11==0)&&(i2+i4+i6+i8+i10+i12==21))
+            cout<<i1<<i2<<i3<<i4<<i5<<i6<<i7<<i8<<i9<<i10<<i11<<12<<endl;
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
 }
+return 0;
+
+
